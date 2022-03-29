89ee30d26a797c1e2e2f0a4015f0ca9f25574368
Amal <amalkrish12@gmail.com>
2022-03-29 12:47:55 +0530

    Routing complete

---
+++ Project/public/Signup-Page.js
@@ -0,0 +1,15 @@
+function check(){
+    let email = $("#email").val();
+    let password = $("#password").val();
+    let confirmPassword = $("#passwordConfirm").val();
+    if(password == confirmPassword && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+        $("#signup-button").removeAttr("disabled");
+    }
+    else {
+        $("#signup-button").prop("disabled",true);
+    }
+}
+
+$(document).ready(function () {
+    $(".modal").modal('show');
+});
